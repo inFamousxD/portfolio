0207a948afb3cc3f6df29b30dbdb495b2fde0661
inFamousxD <audimankar@gmail.com>
2025-04-04 03:39:45 -0400

    Init

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard/Dashboard';
+
+
+const App: React.FC = () => {
+  return (
+      <Router>
+        <Routes>
+          <Route path="/">  
+            <Route index element={<Dashboard />} />
+          </Route>
+        </Routes>
+      </Router>
+  );
+};
+
+export default App;
